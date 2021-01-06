74debd42c765320fae137e90dcccb3a8af334b6c
Ficow <945655645@qq.com>
2021-01-06 08:37:58 +0800

    Move zeroes;

--- LeetCodeProblems-Swift/LeetCodeProblems-Swift/Solutions.swift
+++ LeetCodeProblems-Swift/LeetCodeProblems-Swift/Solutions.swift
@@ -16,7 +16,77 @@ class Solution {
     }
 
     func run() {
-        testContainerWithMostWater()
+        testMoveZeroes()
+    }
+
+    private func testMoveZeroes() {
+        var nums = [2,1,0,0,3]
+//         var nums = [0,1,0,3,12]
+        moveZeroes(&nums)
+        print(nums)
+    }
+
+    // https://leetcode-cn.com/problems/move-zeroes/
+    func moveZeroes(_ nums: inout [Int]) {
+
+        // moving solutions
+
+        var i = 0
+        for j in 0..<nums.count {
+            if nums[j] != 0 {
+                if j > i { // reduce swapping, make this solution fastest
+                    let temp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = temp
+                }
+                i += 1
+            }
+        }
+
+//        var zeroCount = 0
+//        for i in 0..<nums.count {
+//            if nums[i] == 0 {
+//                zeroCount += 1
+//            } else if zeroCount != 0 { // has zero number
+//                nums[i - zeroCount] = nums[i]
+//                nums[i] = 0
+//            }
+//        }
+
+//        var index = 0
+//        // move all the non-zero nums
+//        for i in 0..<nums.count {
+//            if nums[i] != 0 {
+//                nums[index] = nums[i]
+//                index += 1
+//            }
+//        }
+//        // set the rest to zero
+//        while index < nums.count {
+//            nums[index] = 0
+//            index += 1
+//        }
+
+        // my solution
+
+//        var zeroIndex = 0
+//        for index in 0..<nums.count {
+//            if zeroIndex == index {
+//                continue
+//            }
+//            if nums[index] == 0 {
+//                if nums[zeroIndex] != 0 {
+//                    zeroIndex = index
+//                }
+//                continue
+//            }
+//            if nums[zeroIndex] != 0 {
+//                continue
+//            }
+//            nums[zeroIndex] = nums[index]
+//            nums[index] = 0
+//            zeroIndex += 1
+//        }
     }
 
     private func testContainerWithMostWater() {
