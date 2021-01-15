9ec4d008ec9fe25115d575248b9096c5b6051135
Ficow <945655645@qq.com>
2021-01-15 08:53:56 +0800

    Least Recently Used(LRU) cache;

---
+++ LeetCodeProblems-Swift/LeetCodeProblems-Swift/ArrayProblems.swift
@@ -0,0 +1,346 @@
+//
+//  Array.swift
+//  LeetCodeProblems-Swift
+//
+//  Created by Ficow on 2021/1/15.
+//
+
+import Foundation
+
+final class ArrayProblems {
+
+    func run() {
+        testLRUCache()
+    }
+
+    // MARK: - LRUCache
+
+    private func testLRUCache() {
+        /*
+         ["LRUCache","put","put","get","put","get","put","get","get","get"]
+         [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+         result: [null,null,null,1,null,-1,null,-1,3,4]
+         */
+        do {
+            let lRUCache = LRUCache(2);
+            lRUCache.put(1, 1) // 缓存是 {1=1}
+            lRUCache.put(2, 2) // 缓存是 {1=1, 2=2}
+            assert(lRUCache.get(1) == 1)    // 返回 1
+            lRUCache.put(3, 3) // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+            assert(lRUCache.get(2) == -1)    // 返回 -1 (未找到)
+            lRUCache.put(4, 4) // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+            assert(lRUCache.get(1) == -1)    // 返回 -1 (未找到)
+            assert(lRUCache.get(3) == 3)    // 返回 3
+            assert(lRUCache.get(4) == 4)    // 返回 4
+        }
+
+        /*
+         ["LRUCache","put","put","get","get","put","get","get","get"]
+         [[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
+         result: [null,null,null,2,1,null,1,-1,3]
+         */
+        //["LRUCache","put","put","get","get","put","get","get","get"]
+        //[[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
+        //[null,null,null,2,1,null,1,-1,3]
+        do {
+            let lRUCache = LRUCache(2);
+            lRUCache.put(2, 1)
+            lRUCache.put(3, 2)
+            assert(lRUCache.get(3) == 2)
+            assert(lRUCache.get(2) == 1)
+            lRUCache.put(4, 3)
+            assert(lRUCache.get(2) == 1)
+            assert(lRUCache.get(3) == -1)
+            assert(lRUCache.get(4) == 3)
+        }
+    }
+
+    final class LRUCache {
+
+        static let notFound = -1
+
+        private var dict: [Int: Node]
+        private let capacity: Int
+
+        // use dedicated head and tail node to simplify the algorithm
+        private let head = Node(key: 0, value: 0)
+        private let tail = Node(key: 0, value: 0)
+
+        init(_ capacity: Int) {
+            self.capacity = capacity
+            dict = [Int: Node](minimumCapacity: capacity)
+            head.next = tail
+            tail.prev = head
+        }
+
+        func get(_ key: Int) -> Int {
+//            DLog(key)
+            if let node = dict[key] {
+                // get value and update order
+                moveToTail(node: node)
+                return node.value
+            }
+            return LRUCache.notFound
+        }
+
+        func put(_ key: Int, _ value: Int) {
+//            DLog((key, value))
+            if let node = dict[key] {
+                // set value and update order
+                node.value = value
+                moveToTail(node: node)
+            } else {
+                if dict.keys.count >= capacity {
+                    // remove the least recently used value
+                    removeFirst()
+                }
+                // set value and update order
+                let node = Node(key: key, value: value)
+                dict[key] = node
+                moveToTail(node: node)
+            }
+        }
+
+        private func moveToTail(node: Node) {
+            // remove from the current node position
+            let prev = node.prev
+            let next = node.next
+            prev?.next = next
+            next?.prev = prev
+            // append to the tail
+            let last = tail.prev
+            last?.next = node
+            node.prev = last
+            node.next = tail
+            tail.prev = node
+        }
+
+        private func removeFirst() {
+            guard let first = head.next else {
+                return
+            }
+            let second = first.next
+            head.next = second
+            second?.prev = head
+
+            dict[first.key] = nil
+        }
+
+        final class Node {
+            var key: Int
+            var value: Int
+            var prev: Node?
+            var next: Node?
+
+            init(key: Int, value: Int) {
+                self.key = key
+                self.value = value
+            }
+        }
+    }
+
+    // MARK: - threeSum
+
+    private func testThreeSum() {
+        print(threeSum([-1,0,1,2,-1,-4]))
+        print(threeSum([]))
+        print(threeSum([0]))
+        print(threeSum([1,2,-2,-1]))
+    }
+
+    // https://leetcode-cn.com/problems/3sum/
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        // use a sorted array to avoid duplicate results
+        let sortedNums = nums.sorted()
+        let maxIndex = nums.count - 1
+        var results = [[Int]]()
+
+        for first in 0..<nums.count {
+            // if the first number is bigger than 0
+            if sortedNums[first] > 0 { return results }
+            // skip same first numbers
+            if first > 0 && sortedNums[first] == sortedNums[first-1] {
+                continue
+            }
+            let expectedSum = -sortedNums[first]
+            var second = first + 1
+            var third = maxIndex
+            while second < maxIndex {
+                defer {
+                    second += 1
+//                    print(sortedNums[first], sortedNums[second], sortedNums[third], #line)
+                }
+                // skip same second numbers
+                if second > first + 1 && sortedNums[second] == sortedNums[second-1] {
+//                    print(sortedNums[first], sortedNums[second], sortedNums[third], #line)
+                    continue
+                }
+                while second < third && sortedNums[second] + sortedNums[third] > expectedSum {
+                    third -= 1
+//                    print(sortedNums[first], sortedNums[second], sortedNums[third], #line)
+                }
+                // b gets bigger, so c should gets smaller. but b should be bigger than c
+                if second == third {
+                    break
+                }
+                if sortedNums[second] + sortedNums[third] == expectedSum {
+                    results.append([sortedNums[first], sortedNums[second], sortedNums[third]])
+                }
+            }
+        }
+        return results
+    }
+
+    // MARK: - climbStairs
+
+    private func testClimbStairs() {
+        for i in 0..<10 {
+            print(climbStairs(i))
+//            res.removeAll()
+        }
+//        print(climbStairs(38))
+    }
+
+//    var res = [Int]()
+
+    // https://leetcode-cn.com/problems/climbing-stairs/
+    func climbStairs(_ n: Int) -> Int {
+
+        // fibonacci formula
+
+        let sqrt5 = sqrt(5)
+        let divisor = Double(n + 1)
+        let fibn: Double = pow((1 + sqrt5) / 2, divisor) - pow((1 - sqrt5) / 2, divisor)
+        return Int(round(fibn / sqrt5))
+
+        // fibonacci
+
+//        guard n > 1 else {
+//            return 1
+//        }
+//        var x = 1, y = 1, res = 0
+//        for _ in 2...n {
+//            res = x + y
+//            y = x
+//            x = res
+//        }
+//        return res
+
+        // recursion
+
+//        if n < 2 {
+//            return 1
+//        }
+//        if res.isEmpty {
+//            res = [Int](repeating: 0, count: n+1)
+//        }
+//        if res[n] != 0 {
+//            return res[n]
+//        }
+//        res[n] = climbStairs(n - 1) + climbStairs(n - 2)
+//        return res[n]
+    }
+
+    // MARK: - moveZeroes
+
+    private func testMoveZeroes() {
+        var nums = [2,1,0,0,3]
+//         var nums = [0,1,0,3,12]
+        moveZeroes(&nums)
+        print(nums)
+    }
+
+    // https://leetcode-cn.com/problems/move-zeroes/
+    func moveZeroes(_ nums: inout [Int]) {
+
+        // moving solutions
+
+        var i = 0
+        for j in 0..<nums.count {
+            if nums[j] != 0 {
+                if j > i { // reduce swapping, make this solution fastest
+                    let temp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = temp
+                }
+                i += 1
+            }
+        }
+
+//        var zeroCount = 0
+//        for i in 0..<nums.count {
+//            if nums[i] == 0 {
+//                zeroCount += 1
+//            } else if zeroCount != 0 { // has zero number
+//                nums[i - zeroCount] = nums[i]
+//                nums[i] = 0
+//            }
+//        }
+
+//        var index = 0
+//        // move all the non-zero nums
+//        for i in 0..<nums.count {
+//            if nums[i] != 0 {
+//                nums[index] = nums[i]
+//                index += 1
+//            }
+//        }
+//        // set the rest to zero
+//        while index < nums.count {
+//            nums[index] = 0
+//            index += 1
+//        }
+
+        // my solution
+
+//        var zeroIndex = 0
+//        for index in 0..<nums.count {
+//            if zeroIndex == index {
+//                continue
+//            }
+//            if nums[index] == 0 {
+//                if nums[zeroIndex] != 0 {
+//                    zeroIndex = index
+//                }
+//                continue
+//            }
+//            if nums[zeroIndex] != 0 {
+//                continue
+//            }
+//            nums[zeroIndex] = nums[index]
+//            nums[index] = 0
+//            zeroIndex += 1
+//        }
+    }
+
+    // MARK: - maxArea / container with most water
+
+    private func testContainerWithMostWater() {
+//        benchmark {
+            print(maxArea([1,8,6,2,5,4,8,3,7]))
+            print(maxArea([1,1]))
+            print(maxArea([4,3,2,1,4]))
+            print(maxArea([1,2,1]))
+            print(maxArea([2,1]))
+//        }
+    }
+
+    // https://leetcode-cn.com/problems/container-with-most-water/
+    func maxArea(_ height: [Int]) -> Int {
+        var left = 0, right = height.count - 1, result = 0
+        while left < right {
+            let x = right - left
+            let y = min(height[left], height[right])
+            if height[left] < height[right] {
+                left += 1
+            } else if height[left] > height[right] {
+                right -= 1
+            } else {
+                left += 1
+                right -= 1
+            }
+            result = max(result, x * y)
+        }
+        return result
+    }
+}
---
+++ LeetCodeProblems-Swift/LeetCodeProblems-Swift/Helpers.swift
@@ -0,0 +1,24 @@
+//
+//  Helpers.swift
+//  LeetCodeProblems-Swift
+//
+//  Created by Ficow on 2021/1/15.
+//
+
+import Foundation
+
+func DLog<T>(_ msg: T, file: String = #file, function: String = #function, line: Int = #line) {
+    #if DEBUG
+    let file: String = (file as NSString).pathComponents.last ?? "UnknownFile"
+    let time: String = DefaultDateFormatter.string(from: Date())
+    let output: String =  "⚠️ \(time)\n<\(file)>\(function)[\(line)]:\n\(msg)"
+    print(output)
+    #endif
+}
+
+let DefaultDateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat =  "yyyy-MM-dd HH:mm:ss"
+    formatter.timeZone = NSTimeZone.system
+    return formatter
+}()
