e117a9fb62664e77b7fa2a2c585660c080043cab
Ficow <945655645@qq.com>
2021-01-23 13:05:08 +0800

    N-ary Tree Preorder Traversal;

--- LeetCodeProblems-Swift/LeetCodeProblems-Swift/TreeProblems.swift
+++ LeetCodeProblems-Swift/LeetCodeProblems-Swift/TreeProblems.swift
@@ -11,10 +11,54 @@ import Foundation
 final class TreeProblems {
 
     func run() {
-        testPostorderTraversal()
+        testNaryTreePreorderTraversal()
     }
 
-    // MARK: - Traversals
+    // MARK: - N-ary Tree Preorder Traversal
+
+    public class Node {
+        public var val: Int
+        public var children: [Node]
+        public init(_ val: Int, children: [Node] = []) {
+            self.val = val
+            self.children = children
+        }
+    }
+
+    private func testNaryTreePreorderTraversal() {
+        let root = Node(1, children: [
+            Node(3, children: [Node(5), Node(6)]),
+            Node(2),
+            Node(4)
+        ])
+        assert(preorder(root) == [1,3,5,6,2,4])
+    }
+
+    // https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+    func preorder(_ root: Node?) -> [Int] {
+
+        // normal iteration
+        var result = [Int]()
+        guard let node = root else { return result }
+        var stack = [node]
+        while let last = stack.popLast() {
+            result.append(last.val)
+            stack.append(contentsOf: last.children.reversed())
+        }
+        return result
+
+        // normal recursion
+//        var result = [Int]()
+//        func traverse(_ node: Node?) {
+//            guard let node = node else { return }
+//            result.append(node.val)
+//            node.children.forEach { traverse($0) }
+//        }
+//        traverse(root)
+//        return result
+    }
+
+    // MARK: - Binary Traversals
 
     func testPostorderTraversal() {
         var tree = TreeNode(1, nil, .init(2, .init(3), nil))
