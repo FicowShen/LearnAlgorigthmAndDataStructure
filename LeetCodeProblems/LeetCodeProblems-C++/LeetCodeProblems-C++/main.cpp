317ff7f95bcd6bfad388082004b10ab9b4d46397
Ficow <945655645@qq.com>
2021-07-11 12:31:19 +0800

    Use a workspace to manage Swift, ObjC and C++ projects;

---
+++ LeetCodeProblems/LeetCodeProblems-C++/LeetCodeProblems-C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCodeProblems-C++
+//
+//  Created by Ficow on 2021/7/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LeetCodeProblems-Swift
+//
+//  Created by Ficow on 2021/7/11.
+//
+
+import Foundation
+
+print("Hello, World!")
+
