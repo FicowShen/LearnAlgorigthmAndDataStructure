ee9ca1c1b8c253f2185eb1bc21228417e80dbe44
Ficow <945655645@qq.com>
2021-07-14 08:15:33 +0800

    [C++] buildTree with preorder and inorder(iterative with a stack);

--- LeetCodeProblems/LeetCodeProblems-C++/LeetCodeProblems-C++/Common.h
+++ LeetCodeProblems/LeetCodeProblems-C++/LeetCodeProblems-C++/Common.h
@@ -13,6 +13,7 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
--- LeetCodeProblems/LeetCodeProblems-C++/LeetCodeProblems-C++/TreeProblems/TreeProblems.cpp
+++ LeetCodeProblems/LeetCodeProblems-C++/LeetCodeProblems-C++/TreeProblems/TreeProblems.cpp
@@ -9,22 +9,48 @@
 #include <optional>
 
 TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
+TreeNode* buildTree2(vector<int>& preorder, vector<int>& inorder);
 
 void TreeProblems::run() {
     vector<int> preorder{3,9,20,15,7};
     vector<int> inorder{9,3,15,20,7};
-    TreeNode* result = buildTree(preorder, inorder);
+    TreeNode* result = buildTree2(preorder, inorder);
     TreeNode* expected = buildPerfectBinaryTreeWithLevelTraversal("3,9,20,null,null,15,7");
     assert(isIdenticalTree(result, expected));
 
     preorder = {-1};
     inorder = {-1};
-    result = buildTree(preorder, inorder);
+    result = buildTree2(preorder, inorder);
     expected = buildPerfectBinaryTreeWithLevelTraversal("-1");
     assert(isIdenticalTree(result, expected));
 }
 
 // MARK: - buildTree
+
+TreeNode* buildTree2(vector<int>& preorder, vector<int>& inorder) {
+    if (preorder.empty()) { return nullptr; }
+    TreeNode* root = new TreeNode(preorder[0]);
+    stack<TreeNode*> s;
+    s.push(root);
+    int inorderIndex = 0;
+    for (int i = 1; i < preorder.size(); i++) {
+        int preorderValue = preorder[i];
+        TreeNode* node = s.top();
+        if (node->val != inorder[inorderIndex]) {
+            node->left = new TreeNode(preorderValue);
+            s.push(node->left);
+        } else {
+            while (!s.empty() && s.top()->val == inorder[inorderIndex]) {
+                node = s.top();
+                s.pop();
+                inorderIndex++;
+            }
+            node->right = new TreeNode(preorderValue);
+            s.push(node->right);
+        }
+    }
+    return root;
+}
 
 TreeNode* buildNode(vector<int>& preorder, vector<int>& inorder,
                     unordered_map<int, int>& inMap,
