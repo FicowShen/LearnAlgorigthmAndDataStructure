018ac1ecd0eeed97753d1659cf68e662b39eab10
Ficow <945655645@qq.com>
2021-07-28 08:15:15 +0800

    [Swift] maxProfit2, greedy;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DynamicProgrammingProblems/MaxProfit.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DynamicProgrammingProblems/MaxProfit.swift
@@ -25,14 +25,21 @@ extension MaxProfit {
     final class MaxProfit2 {
         static func run() {
             func judge(prices: [Int], expected: Int) {
-                printAndAssert(result: maxProfit(prices), expected: expected)
+                printAndAssert(result: greedy1(prices), expected: expected)
             }
             judge(prices: [7,1,5,3,6,4], expected: 7)
             judge(prices: [1,2,3,4,5], expected: 4)
             judge(prices: [7,6,4,3,1], expected: 0)
         }
 
-        static func maxProfit(_ prices: [Int]) -> Int {
+        
+        static func greedy1(_ prices: [Int]) -> Int {
+            (1..<prices.count).reduce(into: 0) { sum, i in
+                sum += max(prices[i] - prices[i - 1], 0)
+            }
+        }
+
+        static func compressedDP1(_ prices: [Int]) -> Int {
             var hasStock = -prices[0], noStock = 0
             for i in 1..<prices.count {
                 let notBuy = max(noStock, hasStock + prices[i])
@@ -43,16 +50,16 @@ extension MaxProfit {
             return noStock
         }
 
-//        static func maxProfit(_ prices: [Int]) -> Int {
-//            var dp = [[Int]](repeating: [0, 0], count: prices.count)
-//            dp[0][0] = 0
-//            dp[0][1] = -prices[0]
-//            for i in 1..<prices.count {
-//                dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-//                dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
-//            }
-//            return dp[prices.count - 1][0]
-//        }
+        static func rawDP1(_ prices: [Int]) -> Int {
+            var dp = [[Int]](repeating: [0, 0], count: prices.count)
+            dp[0][0] = 0
+            dp[0][1] = -prices[0]
+            for i in 1..<prices.count {
+                dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+                dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+            }
+            return dp[prices.count - 1][0]
+        }
     }
 }
 
