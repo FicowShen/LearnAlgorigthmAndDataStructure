a9970691016093c6d7712631376201e72094a250
Ficow <945655645@qq.com>
2021-08-04 08:51:14 +0800

    [Swift] house robber 3, dfs with memo 3, dp with array/tuple 1 & 2;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DynamicProgrammingProblems/HouseRobber/HouseRobber3.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DynamicProgrammingProblems/HouseRobber/HouseRobber3.swift
@@ -11,13 +11,19 @@ final class HouseRobber3 {
     func run() {
         func judge(_ nodes: [Int?], expected: Int) {
             let tree = perfectBinaryTreeFromLevelTraversal(nodes)
-            printAndAssert(result: robWithMemo2(tree), expected: expected)
+            printAndAssert(result: robWithDP2(tree), expected: expected)
         }
         judge([3,2,3,nil,3,nil,1], expected: 7)
         judge([3,4,5,1,3,nil,1], expected: 9)
     }
 
-    func robWithMemo3(_ root: TreeNode?) -> Int {
+
+
+    func robWithDP4(_ root: TreeNode?) -> Int {
+        fatalError()
+    }
+
+    func robWithMemo4(_ root: TreeNode?) -> Int {
         fatalError()
     }
 
@@ -33,6 +39,37 @@ final class HouseRobber3 {
 
 
 
+    func robWithDP3(_ root: TreeNode?) -> Int {
+
+
+
+
+
+
+
+
+        fatalError()
+    }
+
+
+
+
+    func robWithMemo3(_ root: TreeNode?) -> Int {
+        var memo = [ObjectIdentifier: Int]()
+        func dfs(_ node: TreeNode?) -> Int {
+            guard let node = node else { return 0 }
+            let id = ObjectIdentifier(node)
+            if let value = memo[id] { return value }
+            let current = node.val
+                + (node.left == nil ? 0 : dfs(node.left?.left) + dfs(node.left?.right))
+                + (node.right == nil ? 0 : dfs(node.right?.left) + dfs(node.right?.right))
+            let children = dfs(node.left) + dfs(node.right)
+            let result = max(current, children)
+            memo[id] = result
+            return result
+        }
+        return dfs(root)
+    }
 
 
 
@@ -53,6 +90,31 @@ final class HouseRobber3 {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    func robWithDP2(_ root: TreeNode?) -> Int {
+        func dp(_ node: TreeNode?) -> (Int, Int) {
+            guard let node = node else { return (0, 0) }
+            let left = dp(node.left), right = dp(node.right)
+            let result = (
+                max(left.0, left.1) + max(right.0, right.1),
+                left.0 + right.0 + node.val
+            )
+            return result
+        }
+        let result = dp(root)
+        return max(result.0, result.1)
+    }
 
 
     func robWithMemo2(_ root: TreeNode?) -> Int {
@@ -92,8 +154,23 @@ final class HouseRobber3 {
 
 
 
-
-
+    // https://leetcode-cn.com/problems/house-robber-iii/solution/san-chong-fang-fa-jie-jue-shu-xing-dong-tai-gui-hu/
+    func robWithDP1(_ root: TreeNode?) -> Int {
+        func dp(_ root: TreeNode?) -> [Int] {
+            guard let root = root else { return [0, 0] }
+            var result = [0, 0]
+            //自底向上
+            let left = dp(root.left)
+            let right = dp(root.right)
+            //不抢，下家可能抢可能不抢，取决于收益大小
+            result[0] = max(left[0], left[1]) + max(right[0], right[1])
+            //抢，下家就不能抢了
+            result[1] = root.val + left[0] + right[0]
+            return result
+        }
+        let result = dp(root)
+        return max(result[0], result[1])
+    }
 
 
     func robWithMemo1(_ root: TreeNode?) -> Int {
