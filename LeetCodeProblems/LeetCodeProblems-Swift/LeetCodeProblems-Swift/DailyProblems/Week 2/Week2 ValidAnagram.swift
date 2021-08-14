746980c8db07f712fbd11b91101143d896dabe8f
Ficow <945655645@qq.com>
2021-08-14 10:29:49 +0800

    [Swift] valid anagram, dict & ascii array & letter array, 3;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 2/Week2 ValidAnagram.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 2/Week2 ValidAnagram.swift
@@ -10,22 +10,87 @@ import Foundation
 // https://leetcode-cn.com/problems/valid-anagram/
 final class Week2ValidAnagram {
     func run() {
-        let f = isAnagramWithLetterArray2
+        let f = isAnagramWithLetterArray3
         printAndAssert(result: f("anagram", "nagaram"), expected: true)
         printAndAssert(result: f("rat", "car"), expected: false)
     }
 
 
-    func isAnagramWithLetterArray3(_ s: String, _ t: String) -> Bool {
+
+    func isAnagramWithLetterArray4(_ s: String, _ t: String) -> Bool {
         fatalError()
+    }
+
+    func isAnagramWithASCIIArray4(_ s: String, _ t: String) -> Bool {
+        fatalError()
+    }
+
+    func isAnagramWithDict4(_ s: String, _ t: String) -> Bool {
+        fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    func isAnagramWithLetterArray3(_ s: String, _ t: String) -> Bool {
+        if s.count != t.count { return false }
+        let a = Character("a").asciiValue!
+        var array = [Int16](repeating: 0, count: 26)
+        for c in s {
+            let i = Int(c.asciiValue! - a)
+            array[i] += 1
+        }
+        for c in t {
+            let i = Int(c.asciiValue! - a)
+            array[i] -= 1
+            if array[i] < 0 { return false }
+        }
+        return true
     }
 
     func isAnagramWithASCIIArray3(_ s: String, _ t: String) -> Bool {
-        fatalError()
+        if s.count != t.count { return false }
+        var array = [Int16](repeating: 0, count: 255)
+        for c in s {
+            let i = Int(c.asciiValue!)
+            array[i] += 1
+        }
+        for c in t {
+            let i = Int(c.asciiValue!)
+            array[i] -= 1
+            if array[i] < 0 { return false }
+        }
+        return true
     }
 
     func isAnagramWithDict3(_ s: String, _ t: String) -> Bool {
-        fatalError()
+        if s.count != t.count { return false }
+        var dict = [String.Element: Int16]()
+        for c in s {
+            dict[c, default: 0] += 1
+        }
+        for c in t {
+            dict[c, default: 0] -= 1
+            if dict[c]! < 0 { return false }
+        }
+        return true
     }
 
 
