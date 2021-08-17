b6545b9dcae37795a019e22d74f602948ffa106a
Ficow <945655645@qq.com>
2021-08-17 22:38:49 +0800

    [Swift] max depth of binary tree, 3;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 2/Day13 MaximumDepthOfBinaryTree.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 2/Day13 MaximumDepthOfBinaryTree.swift
@@ -10,7 +10,7 @@ import Foundation
 // https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
 final class Day13MaximumDepthOfBinaryTree {
     func run() {
-        let f = maxDepthWithBFS2
+        let f = maxDepthWithBFS3
         func judge(nodes: [Int?], expected: Int) {
             let root = TreeNode.fromPerfectBinaryTreeLevelNodes(nodes)
             printAndAssert(result: f(root), expected: expected)
@@ -20,16 +20,68 @@ final class Day13MaximumDepthOfBinaryTree {
         judge(nodes: [], expected: 0)
     }
 
-    func maxDepthWithBFS3(_ root: TreeNode?) -> Int {
+
+
+    func maxDepthWithBFS4(_ root: TreeNode?) -> Int {
         fatalError()
     }
 
-    func maxDepthWithoutMaxFunction3(_ root: TreeNode?) -> Int {
+    func maxDepthWithoutMaxFunction4(_ root: TreeNode?) -> Int {
         fatalError()
+    }
+
+    func maxDepthShortest4(_ root: TreeNode?) -> Int {
+        fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    func maxDepthWithBFS3(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        var level = 0, q = [root]
+        while !q.isEmpty {
+            var nodes = [TreeNode]()
+            for node in q {
+                if let l = node.left { nodes.append(l) }
+                if let r = node.right { nodes.append(r) }
+            }
+            q = nodes
+            level += 1
+        }
+        return level
+    }
+
+    func maxDepthRecurseWithLevel3(_ root: TreeNode?) -> Int {
+        var maxLevel = 0
+        func f(_ root: TreeNode?, level: Int) {
+            guard let root = root else { return }
+            maxLevel = max(maxLevel, level)
+            f(root.left, level: level + 1)
+            f(root.right, level: level + 1)
+        }
+        f(root, level: 1)
+        return maxLevel
     }
 
     func maxDepthShortest3(_ root: TreeNode?) -> Int {
-        fatalError()
+        func f(_ root: TreeNode?) -> Int {
+            root == nil ? 0 : max(f(root?.left), f(root?.right)) + 1
+        }
+        return f(root)
     }
 
 
