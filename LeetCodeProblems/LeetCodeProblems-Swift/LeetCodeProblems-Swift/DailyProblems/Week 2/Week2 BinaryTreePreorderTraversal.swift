cc8fa3d9aa143486d0cff64d2b3da41bcc62c4b2
Ficow <945655645@qq.com>
2021-08-18 09:40:07 +0800

    [Swift] binary tree preorder traversal, recursion & stack & visit stack & generic stack & morris, 2;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 2/Week2 BinaryTreePreorderTraversal.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 2/Week2 BinaryTreePreorderTraversal.swift
@@ -10,7 +10,7 @@ import Foundation
 // https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
 final class Week2BinaryTreePreorderTraversal {
     func run() {
-        let f = preorderTraversalWithMorris1_1
+        let f = preorderTraversalWithMorris2
         func judge(nodes: [Int?], expected: [Int]) {
             let root = TreeNode.fromPerfectBinaryTreeLevelNodes(nodes)
             printAndAssert(result: f(root), expected: expected)
@@ -21,24 +21,118 @@ final class Week2BinaryTreePreorderTraversal {
         judge(nodes: [1,2,3], expected: [1,2,3])
     }
 
-    func preorderTraversalWithMorris2(_ root: TreeNode?) -> [Int] {
+
+
+    func preorderTraversalWithMorris3(_ root: TreeNode?) -> [Int] {
         fatalError()
+    }
+
+    func preorderTraversalWithGenericStack3(_ root: TreeNode?) -> [Int] {
+        fatalError()
+    }
+
+    func preorderTraversalWithVisitStack3(_ root: TreeNode?) -> [Int] {
+        fatalError()
+    }
+
+    func preorderTraversalWithStack3(_ root: TreeNode?) -> [Int] {
+        fatalError()
+    }
+
+    func preorderTraversalWithRecursion3(_ root: TreeNode?) -> [Int] {
+        fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+    func preorderTraversalWithMorris2(_ root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        var ans = [Int](), p1: TreeNode! = root, p2: TreeNode!
+        while p1 != nil {
+            p2 = p1.left
+            if p2 == nil {
+                ans.append(p1.val)
+                p1 = p1.right
+                continue
+            }
+            while p2.right != nil, p2.right !== p1 {
+                p2 = p2.right
+            }
+            if p2.right == nil {
+                ans.append(p1.val)
+                p2.right = p1
+                p1 = p1.left
+            } else {
+                p2.right = nil
+                p1 = p1.right
+            }
+        }
+        return ans
     }
 
     func preorderTraversalWithGenericStack2(_ root: TreeNode?) -> [Int] {
-        fatalError()
+        guard let root = root else { return [] }
+        var ans = [Int](), stack: [Any] = [root]
+        while let element = stack.popLast() {
+            if let value = element as? Int {
+                ans.append(value)
+                continue
+            }
+            let node = element as! TreeNode
+            if let r = node.right { stack.append(r) }
+            if let l = node.left { stack.append(l) }
+            stack.append(node.val)
+        }
+        return ans
     }
 
-    func preorderTraversalWithTaggedStack2(_ root: TreeNode?) -> [Int] {
-        fatalError()
+    func preorderTraversalWithVisitStack2(_ root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        var ans = [Int](), stack = [(visited: false, node: root)]
+        while let element = stack.popLast() {
+            if element.visited {
+                ans.append(element.node.val)
+                continue
+            }
+            let node = element.node
+            if let r = node.right { stack.append((false, r)) }
+            if let l = node.left { stack.append((false, l)) }
+            stack.append((true, node))
+        }
+        return ans
     }
 
     func preorderTraversalWithStack2(_ root: TreeNode?) -> [Int] {
-        fatalError()
+        var ans = [Int](), stack = [TreeNode](), p: TreeNode! = root
+        while !stack.isEmpty || p != nil {
+            while p != nil {
+                ans.append(p.val)
+                stack.append(p)
+                p = p.left
+            }
+            let node = stack.popLast()!
+            p = node.right
+        }
+        return ans
     }
 
     func preorderTraversalWithRecursion2(_ root: TreeNode?) -> [Int] {
-        fatalError()
+        var ans = [Int]()
+        func f(_ root: TreeNode?) {
+            guard let root = root else { return }
+            ans.append(root.val)
+            f(root.left)
+            f(root.right)
+        }
+        f(root)
+        return ans
     }
 
 
