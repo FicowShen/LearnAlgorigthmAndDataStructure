7d63e103f56abd344c08c2e59f38e3c831f58604
Ficow <945655645@qq.com>
2021-08-27 07:42:44 +0800

    sqrt x, binary search & newton iteration, 3;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 4/Week4 SqrtX.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 4/Week4 SqrtX.swift
@@ -18,18 +18,59 @@ import Foundation
  */
 final class Week4SqrtX {
     func run() {
-        let f = newtonIteration2
+        let f = newtonIteration3
         printAndAssert(result: f(4), expected: 2)
         printAndAssert(result: f(8), expected: 2)
     }
 
 
-    func newtonIteration3(_ x: Int) -> Int {
+    func newtonIteration4(_ x: Int) -> Int {
         fatalError()
     }
 
-    func binarySearch3(_ x: Int) -> Int {
+    func binarySearch4(_ x: Int) -> Int {
         fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    func newtonIteration3(_ x: Int) -> Int {
+        var r = x
+        while r * r > x {
+            r = (r + x/r) >> 1
+        }
+        return r
+    }
+
+    func binarySearch3(_ x: Int) -> Int {
+        var l = 0, r = x, ans = -1
+        while l <= r {
+            let mid = l + (r - l) >> 1
+            if mid * mid <= x {
+                ans = mid
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        return ans
     }
 
 
