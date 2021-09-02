802ab9235dc4015ed5fbc7ca7f5f216bdcbab9ae
Ficow <945655645@qq.com>
2021-09-02 08:21:33 +0800

    revise word ladder ii, 2 & 3;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 4/Week4 WordLadderII.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 4/Week4 WordLadderII.swift
@@ -12,7 +12,7 @@ import Foundation
  */
 final class Week4WordLadderII {
     func run() {
-        let f = BFS1
+        let f = bfs3
         func judge(_ begin: String, _ end: String, _ wordList: [String], expected: [[String]]) {
             printAndAssert(result: Set(f(begin, end, wordList)), expected: Set(expected))
         }
@@ -23,12 +23,125 @@ final class Week4WordLadderII {
     }
 
 
-    func BFS2(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
+    func bfs5(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
         fatalError()
     }
 
+
+    func bfs4(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
+        fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    func bfs3(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
+        let letters = (0..<26).map { Character(Unicode.Scalar($0 + 97)) }
+        var valid = Set(wordList), visited = Set<String>()
+        var q = [[beginWord]], ans = [[String]](), minLevel = Int.max, level = 1
+        while !q.isEmpty {
+            var next = [[String]]()
+            for path in q {
+                if path.count > minLevel { continue }
+                let lastWord = Array(path.last!)
+                for i in 0..<lastWord.count {
+                    for letter in letters {
+                        if lastWord[i] == letter { continue }
+                        var temp = lastWord
+                        temp[i] = letter
+                        let newWord = String(temp)
+                        if !valid.contains(newWord) { continue }
+                        visited.insert(newWord)
+                        var newPath = path
+                        newPath.append(newWord)
+                        if newWord == endWord {
+                            minLevel = level
+                            ans.append(newPath)
+                        } else {
+                            next.append(newPath)
+                        }
+                    }
+                }
+            }
+            visited.forEach { valid.remove($0) }
+            q = next
+            level += 1
+        }
+        return ans
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    func bfs2(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
+        let letters = (0..<26).map { Character(Unicode.Scalar(97 + $0)) }
+        var valid = Set(wordList), visited = Set<String>(), q = [[String]]()
+        var ans = [[String]]()
+        q.append([beginWord])
+        while !q.isEmpty {
+            var newQ = [[String]]()
+            for path in q {
+                let lastWord = Array(path.last!)
+                for i in 0..<lastWord.count {
+                    for letter in letters {
+                        if lastWord[i] == letter { continue }
+                        var temp = lastWord
+                        temp[i] = letter
+                        let newWord = String(temp)
+                        if !valid.contains(newWord) { continue }
+                        visited.insert(newWord)
+                        var newPath = path
+                        newPath.append(newWord)
+                        if newWord == endWord {
+                            ans.append(newPath)
+                        } else {
+                            newQ.append(newPath)
+                        }
+                    }
+                }
+            }
+            for v in visited { valid.remove(v) }
+            q = newQ
+        }
+        return ans
+    }
+
+
+
+
+
+
     // https://leetcode.com/problems/word-ladder-ii/discuss/40434/C++-solution-using-standard-BFS-method-no-DFS-or-backtracking/812423
-    func BFS1(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
+    func bfs1(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
         let letters = (0..<26).map { Character(Unicode.Scalar(97 + $0)) }
         var st = Set(wordList), visited = Set<String>(), ans = [[String]]()
         var q = [[String]]()
