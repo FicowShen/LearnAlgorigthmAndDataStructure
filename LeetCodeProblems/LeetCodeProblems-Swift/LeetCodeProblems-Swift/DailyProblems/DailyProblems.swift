57840f58d72f809292a6a595be511267af0692d5
Ficow <945655645@qq.com>
2021-09-09 09:30:39 +0800

    triangle, dp, 1 & 2;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/DailyProblems.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/DailyProblems.swift
@@ -18,6 +18,7 @@ final class DailyProblems {
 
     /// dynamic programming
     static func week5And6() {
+        Week6Triangle().run()
         Week6UniquePathsII().run()
         Week6UniquePaths().run()
         Week6ClimbStairs().run()
---
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 Triangle.swift
@@ -0,0 +1,81 @@
+//
+//  Week6 Triangle.swift
+//  LeetCodeProblems-Swift
+//
+//  Created by Ficow on 2021/9/9.
+//
+
+import Foundation
+
+/*
+ https://leetcode-cn.com/problems/triangle/description/
+ Should consider 2 questions?
+ a. Any limit for using extra space?
+ b. Allow to modify the triangle array?
+
+ 1. dp
+ Time: O(n^2), Space: O(n)
+ */
+final class Week6Triangle {
+    func run() {
+        let f = dp2
+        printAndAssert(result: f([[2],[3,4]]), expected: 5)
+        printAndAssert(result: f([[2],[3,4],[6,5,7],[4,1,8,3]]), expected: 11)
+        printAndAssert(result: f([[-10]]), expected: -10)
+    }
+
+
+    func dp5(_ triangle: [[Int]]) -> Int {
+        fatalError()
+    }
+
+
+    func dp4(_ triangle: [[Int]]) -> Int {
+        fatalError()
+    }
+
+
+    func dp3(_ triangle: [[Int]]) -> Int {
+        fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    func dp2(_ triangle: [[Int]]) -> Int {
+        var dp = triangle.last!
+        for i in stride(from: triangle.count - 2, through: 0, by: -1) {
+            for j in 0..<triangle[i].count {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+            }
+        }
+        return dp[0]
+    }
+
+
+
+
+
+
+
+    func dp1(_ triangle: [[Int]]) -> Int {
+        let row = triangle.count
+        var dp = triangle.last!
+        for i in stride(from: row - 2, through: 0, by: -1) {
+            for j in 0..<triangle[i].count {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+            }
+        }
+        return dp[0]
+    }
+}
