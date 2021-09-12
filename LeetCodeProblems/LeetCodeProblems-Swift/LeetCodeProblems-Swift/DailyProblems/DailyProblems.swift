643271df2bbcd47886dc2653521cdfaa3d0a4ad3
Ficow <945655645@qq.com>
2021-09-12 22:14:36 +0800

    decode ways, dp;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/DailyProblems.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/DailyProblems.swift
@@ -18,6 +18,7 @@ final class DailyProblems {
 
     /// dynamic programming
     static func week5And6() {
+        Week6DecodeWays().run()
         Week6BestTimeToBuyAndSellStockIV().run()
         Week6BestTimeToBuyAndSellStockIII().run()
         Week6BestTimeToBuyAndSellStockWithTransactionFee().run()
---
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 DecodeWays.swift
@@ -0,0 +1,101 @@
+//
+//  Week6 DecodeWays.swift
+//  LeetCodeProblems-Swift
+//
+//  Created by Ficow on 2021/9/12.
+//
+
+import Foundation
+
+/*
+ https://leetcode-cn.com/problems/decode-ways/
+ 1. dp
+ Time: O(n), Space: O(n) / O(1)
+ use 1 letter and use 2 letters, empty string has one way to decode
+ */
+final class Week6DecodeWays {
+    func run() {
+        let f = dp1
+        printAndAssert(result: f("12"), expected: 2)
+        printAndAssert(result: f("226"), expected: 3)
+        printAndAssert(result: f("0"), expected: 0)
+        printAndAssert(result: f("06"), expected: 0)
+    }
+
+
+
+
+    func dp3(_ s: String) -> Int {
+        fatalError()
+    }
+
+    func rawDP3(_ s: String) -> Int {
+        fatalError()
+    }
+
+
+
+
+    func dp2(_ s: String) -> Int {
+        fatalError()
+    }
+
+    func rawDP2(_ s: String) -> Int {
+        fatalError()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    func dp1(_ s: String) -> Int {
+        let n = s.count, s = Array(s)
+        // a = f[i-2], b = f[i-1], c = f[i]
+        var a = 0, b = 1, c = 0
+        func isValidLetter(pre: Character, cur: Character) -> Bool {
+            let zero = 48 as UInt8
+            return ((pre.asciiValue! - zero) * 10 + (cur.asciiValue! - zero)) <= 26
+        }
+        for i in 1...n {
+            c = 0
+            if s[i - 1] != "0" {
+                c += b
+            }
+            if i > 1, s[i - 2] != "0", isValidLetter(pre: s[i - 2], cur: s[i - 1]) {
+                c += a
+            }
+            (a, b) = (b, c)
+        }
+        return c
+    }
+
+    func rawDP1(_ s: String) -> Int {
+        let n = s.count, s = Array(s)
+        var f = [Int](repeating: 0, count: n + 1)
+        f[0] = 1 // empty string can be decoded in one way -> empty string
+        func isValidLetter(pre: Character, cur: Character) -> Bool {
+            let zeroScalar: UInt8 = 48, preScalar = pre.asciiValue!, curScalar = cur.asciiValue!
+            return ((preScalar - zeroScalar) * 10 + (curScalar - zeroScalar)) <= 26
+        }
+        for i in 1...n {
+            let cur = s[i - 1]
+            if cur != "0" { // use one letter
+                f[i] += f[i - 1]
+            }
+            if i == 1 { continue }
+            let pre = s[i - 2] // use two letters
+            if pre != "0", isValidLetter(pre: pre, cur: cur) {
+                f[i] += f[i - 2]
+            }
+        }
+        return f[n]
+    }
+}
