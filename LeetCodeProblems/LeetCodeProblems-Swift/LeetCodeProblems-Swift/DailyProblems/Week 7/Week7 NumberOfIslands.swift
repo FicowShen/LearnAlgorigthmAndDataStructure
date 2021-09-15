0c0833901834c7ab53482e65fbeabd8b3bf5e10c
Ficow <945655645@qq.com>
2021-09-15 08:39:52 +0800

    revise number of island;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 7/Week7 NumberOfIslands.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 7/Week7 NumberOfIslands.swift
@@ -11,10 +11,16 @@ import Foundation
  https://leetcode-cn.com/problems/number-of-islands/
  1. Union-Find
  Time: O(m * n), Space: O(m * n)
+
+ 2. bfs (Week4NumberOfIslands)
+ Time: O(m * n), Space: O(m * n)
+
+ 3. dfs (Week4NumberOfIslands)
+ Time: O(m * n), Space: O(m * n)
  */
 final class Week7NumberOfIslands {
     func run() {
-        let f = unionFind1
+        let f = unionFind2
         func judge(_ grid: [[Character]], _ output: Int) {
             printAndAssert(result: f(grid), expected: output)
         }
@@ -51,8 +57,64 @@ final class Week7NumberOfIslands {
 
 
 
+
+
+
+
     func unionFind2(_ grid: [[Character]]) -> Int {
-        fatalError()
+        struct UnionFind {
+            private(set) var count: Int
+            private var parent: [Int]
+            private var size: [Int]
+            init(n: Int) {
+                count = n
+                parent = [Int](repeating: 0, count: n)
+                size = parent
+                for i in 0..<n { parent[i] = i }
+            }
+            mutating func unite(_ p: Int, _ q: Int) {
+                let a = parent(p), b = parent(q)
+                if a == b { return }
+                if size[a] > size[b] {
+                    parent[b] = a
+                    size[a] += size[b]
+                } else {
+                    parent[a] = b
+                    size[b] += size[a]
+                }
+                count -= 1
+            }
+            mutating func isConnected(_ p: Int, _ q: Int) -> Bool {
+                parent(p) == parent(q)
+            }
+            private mutating func parent(_ x: Int) -> Int {
+                var x = x
+                while parent[x] != x {
+                    parent[x] = parent[parent[x]]
+                    x = parent[x]
+                }
+                return x
+            }
+        }
+        let Row = grid.count, Col = grid[0].count
+        let drow = [-1, 0, 1, 0], dcol = [0, 1, 0, -1]
+        var uf = UnionFind(n: Row * Col), zeroes = 0, grid = grid
+        for row in 0..<Row {
+            for col in 0..<Col {
+                if grid[row][col] != "1" {
+                    zeroes += 1
+                    continue
+                }
+                grid[row][col] = "0"
+                for i in 0..<drow.count {
+                    let r = row + drow[i], c = col + dcol[i]
+                    if r < 0 || c < 0 || r >= Row || c >= Col { continue }
+                    if  grid[r][c] != "1" { continue }
+                    uf.unite(row * Col + col, r * Col + c)
+                }
+            }
+        }
+        return uf.count - zeroes
     }
 
 
