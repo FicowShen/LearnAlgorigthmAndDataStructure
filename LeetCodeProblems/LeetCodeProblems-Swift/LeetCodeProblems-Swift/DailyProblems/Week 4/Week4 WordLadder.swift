004accba9aba48c16b954992f7b6225cabff970f
Ficow <945655645@qq.com>
2021-09-17 08:16:29 +0800

    revise word ladder;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 4/Week4 WordLadder.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 4/Week4 WordLadder.swift
@@ -17,7 +17,7 @@ import Foundation
  */
 final class Week4WordLadder {
     func run() {
-        let f = bfs4
+        let f = bfs5
         func judge(_ begin: String, _ end: String, _ list: [String], expected: Int) {
             printAndAssert(result: f(begin, end, list), expected: expected)
         }
@@ -27,6 +27,57 @@ final class Week4WordLadder {
         judge("hot", "dog", ["hot","cog","dog","tot","hog","hop","pot","dot"], expected: 3)
         judge("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"], expected: 5)
     }
+
+
+
+
+    func bfs7(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
+        fatalError()
+    }
+
+
+
+
+    func bfs6(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
+        fatalError()
+    }
+
+
+
+
+
+    func bfs5(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
+        if !wordList.contains(endWord) { return 0 }
+        let letters = (0..<26).map { Character(UnicodeScalar($0 + 97)) }
+        var begin = Set([beginWord]), end = Set([endWord]), valid = Set(wordList)
+        var ans = 1
+        while !begin.isEmpty {
+            if begin.count > end.count {
+                (begin, end) = (end, begin)
+            }
+            var next = Set<String>()
+            for b in begin {
+                let beginChars = Array(b)
+                for i in 0..<b.count {
+                    for letter in letters {
+                        if beginChars[i] == letter { continue }
+                        var temp = beginChars
+                        temp[i] = letter
+                        let new = String(temp)
+                        if end.contains(new) { return ans + 1 }
+                        if !valid.contains(new) { continue }
+                        valid.remove(new)
+                        next.insert(new)
+                    }
+                }
+            }
+            begin = next
+            ans += 1
+        }
+        return 0
+    }
+
+
 
 
 
