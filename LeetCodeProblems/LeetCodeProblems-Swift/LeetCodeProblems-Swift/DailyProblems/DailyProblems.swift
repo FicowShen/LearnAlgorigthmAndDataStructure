eb846e35342e6353bac2ba8a61fc088343bd63e6
Ficow <945655645@qq.com>
2021-09-25 22:22:45 +0800

    n queens ii;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/DailyProblems.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/DailyProblems.swift
@@ -19,6 +19,7 @@ final class DailyProblems {
     }
 
     static func week8() {
+        Week8NQueensII().run()
         Week8CountingBits().run()
         Week8ReversePairs().run()
         Week8MergeIntervals().run()
---
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week8/Week8 NQueensII.swift
@@ -0,0 +1,100 @@
+//
+//  Week8 NQueensII.swift
+//  LeetCodeProblems-Swift
+//
+//  Created by Ficow on 2021/9/25.
+//
+
+import Foundation
+
+/*
+ https://leetcode-cn.com/problems/n-queens-ii/
+ 1. backtrack with set
+ 2. backtrack with bits
+ Time: O(n!), Space: O(n)
+ */
+final class Week8NQueensII {
+    func run() {
+        let f = totalNQueens2
+        printAndAssert(result: f(1), expected: 1)
+        printAndAssert(result: f(4), expected: 2)
+    }
+
+
+
+
+    func totalNQueens4(_ n: Int) -> Int {
+        fatalError()
+    }
+
+
+
+
+
+    
+    func totalNQueens3(_ n: Int) -> Int {
+        fatalError()
+    }
+
+
+
+
+
+
+    func totalNQueens2(_ n: Int) -> Int {
+        func bitCount(_ x: Int) -> Int {
+            var x = x, count = 0
+            while x != 0 {
+                x &= (x - 1)
+                count += 1
+            }
+            return count
+        }
+        func backtrack(row: Int, cols: Int, diagonals1: Int, diagonals2: Int)
+        -> Int {
+            if row == n { return 1 }
+            let d1 = diagonals1, d2 = diagonals2
+            var validPos = ((1 << n) - 1) & (~(cols | d1 | d2)), ans = 0
+            while validPos != 0 {
+                let pos = validPos & -validPos
+                validPos &= (validPos - 1)
+                ans += backtrack(row: row + 1,
+                                 cols: cols | pos,
+                                 diagonals1: (d1 | pos) << 1,
+                                 diagonals2: (d2 | pos) >> 1)
+            }
+            return ans
+        }
+        return backtrack(row: 0, cols: 0, diagonals1: 0, diagonals2: 0)
+    }
+
+
+
+
+    func totalNQueens1(_ n: Int) -> Int {
+        func bitCount(_ x: Int) -> Int {
+            var x = x, count = 0
+            while x != 0 {
+                x &= (x - 1)
+                count += 1
+            }
+            return count
+        }
+        func backtrack(row: Int, cols: Int, diagonals1: Int, diagonals2: Int) -> Int {
+            if row == n { return 1 }
+            let d1 = diagonals1, d2 = diagonals2
+            var validPos = ((1 << n) - 1) & (~(cols | d1 | d2))
+            var ans = 0
+            while validPos != 0 {
+                let pos = validPos & (-validPos)
+                validPos &= (validPos - 1)
+                ans += backtrack(row: row + 1,
+                                 cols: cols | pos,
+                                 diagonals1: (d1 | pos) << 1,
+                                 diagonals2: (d2 | pos) >> 1)
+            }
+            return ans
+        }
+        return backtrack(row: 0, cols: 0, diagonals1: 0, diagonals2: 0)
+    }
+}
