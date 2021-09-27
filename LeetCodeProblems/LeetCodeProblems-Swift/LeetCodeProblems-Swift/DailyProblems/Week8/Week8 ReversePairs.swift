c8d92d7463649f4081ec8dc7bb7411efa594c6ca
Ficow <945655645@qq.com>
2021-09-27 09:31:12 +0800

    revise;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week8/Week8 ReversePairs.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week8/Week8 ReversePairs.swift
@@ -14,9 +14,15 @@ import Foundation
  */
 final class Week8ReversePairs {
     func run() {
-        let f = mergeSort3
+        let f = mergeSort4
         printAndAssert(result: f([1,3,2,3,1]), expected: 2)
         printAndAssert(result: f([2,4,3,5,1]), expected: 3)
+    }
+
+
+
+    func mergeSort6(_ nums: [Int]) -> Int {
+        fatalError()
     }
 
 
@@ -28,8 +34,59 @@ final class Week8ReversePairs {
 
 
     func mergeSort4(_ nums: [Int]) -> Int {
-        fatalError()
+        if nums.isEmpty { return 0 }
+        var nums = nums
+        func reversePairs(left: Int, right: Int) -> Int {
+            if left == right { return 0 }
+            let mid = (left + right) >> 1
+            let leftPairs = reversePairs(left: left, right: mid)
+            let rightPairs = reversePairs(left: mid + 1, right: right)
+            var pairs = leftPairs + rightPairs
+            // now, left...right is sorted by reversePairs
+            var i = left, j = mid + 1
+            while i <= mid {
+                while j <= right, nums[i] > 2 * nums[j] { j += 1 }
+                pairs += (j - mid - 1)
+                i += 1
+            }
+            // merge two parts
+            var sorted = [Int](repeating: 0, count: right - left + 1)
+            var p = 0, p1 = left, p2 = mid + 1
+            while p1 <= mid || p2 <= right {
+                defer { p += 1 }
+                if p1 > mid {
+                    sorted[p] = nums[p2]
+                    p2 += 1
+                } else if p2 > right {
+                    sorted[p] = nums[p1]
+                    p1 += 1
+                } else {
+                    if nums[p1] < nums[p2] {
+                        sorted[p] = nums[p1]
+                        p1 += 1
+                    } else {
+                        sorted[p] = nums[p2]
+                        p2 += 1
+                    }
+                }
+            }
+            for i in 0..<sorted.count {
+                nums[left + i] = sorted[i]
+            }
+            return pairs
+        }
+        return reversePairs(left: 0, right: nums.count - 1)
     }
+
+
+
+
+
+
+
+
+
+
 
 
 
