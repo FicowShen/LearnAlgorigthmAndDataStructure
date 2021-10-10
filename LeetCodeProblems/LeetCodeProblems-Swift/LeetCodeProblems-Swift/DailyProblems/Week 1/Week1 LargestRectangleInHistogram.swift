681dc674c77d3a137bc5a921f0e054c0359c6a34
Ficow <945655645@qq.com>
2021-10-10 18:47:48 +0800

    revise;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 1/Week1 LargestRectangleInHistogram.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 1/Week1 LargestRectangleInHistogram.swift
@@ -19,10 +19,13 @@ import Foundation
  */
 final class Week1LargestRectangleInHistogram {
     func run() {
-        let f = lessFromLeftRightArray2
+        let f = lessFromLeftRightArray3
         printAndAssert(result: f([2,1,5,6,2,3]), expected: 10)
         printAndAssert(result: f([2,4]), expected: 4)
     }
+
+
+
 
 
 
@@ -36,6 +39,9 @@ final class Week1LargestRectangleInHistogram {
 
 
 
+
+
+
     func monotonicStack4(_ heights: [Int]) -> Int {
         fatalError()
     }
@@ -46,12 +52,52 @@ final class Week1LargestRectangleInHistogram {
 
 
 
+
+
+
     func monotonicStack3(_ heights: [Int]) -> Int {
-        fatalError()
+        let heights = [0] + heights + [0] // avoid checking edge indexes
+        var area = 0, stack = [0] // sentinel, stack cannot be empty
+        for i in 1..<heights.count {
+            while heights[i] < heights[stack.last!] {
+                let h = heights[stack.popLast()!]
+                let w = i - stack.last! - 1
+                area = max(area, h * w)
+            }
+            stack.append(i)
+        }
+        return area
     }
 
     func lessFromLeftRightArray3(_ heights: [Int]) -> Int {
-        fatalError()
+        // 2,1,5,6,2,3
+        //   ^     ^
+        //   l     r
+        let n = heights.count
+        var lessFromLeft = [Int](repeating: 0, count: n),
+            lessFromRight = lessFromLeft
+        // invalid indices
+        lessFromLeft[0] = -1
+        lessFromRight[n - 1] = n
+        for i in 1..<heights.count {
+            var p = i - 1
+            while p >= 0, heights[p] >= heights[i] {
+                p = lessFromLeft[p] // get the index of a lower height
+            }
+            lessFromLeft[i] = p
+        }
+        for i in stride(from: n - 2, through: 0, by: -1) {
+            var p = i + 1
+            while p < n, heights[p] >= heights[i] {
+                p = lessFromRight[p] // get the index of a lower height
+            }
+            lessFromRight[i] = p
+        }
+        var area = 0
+        for i in 0..<n{
+            area = max(area, heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1))
+        }
+        return area
     }
 
 
@@ -100,8 +146,9 @@ final class Week1LargestRectangleInHistogram {
     func lessFromLeftRightArray2(_ heights: [Int]) -> Int {
         if heights.isEmpty { return 0 }
         let n = heights.count
-        var lessFromLeft = [Int](repeating: 0, count: n),
-            lessFromRight = lessFromLeft
+        // https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28902/5ms-O(n)-Java-solution-explained-(beats-96)/191784
+        var lessFromLeft = [Int](repeating: 0, count: n), // idx of the first bar the left that is lower than current
+            lessFromRight = lessFromLeft // idx of the first bar the right that is lower than current
         lessFromLeft[0] = -1
         lessFromRight[n - 1] = n
         for i in 1..<n {
@@ -130,7 +177,7 @@ final class Week1LargestRectangleInHistogram {
     // https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/bao-li-jie-fa-zhan-by-liweiwei1419/
     func monotonicStack1(_ heights: [Int]) -> Int {
         if heights.isEmpty { return 0 }
-        let heights = [0] + heights + [0]
+        let heights = [0] + heights + [0] // avoid checking edge indexes
         let n = heights.count
         var area = 0, stack = [Int]()
         stack.append(0) // sentinel
