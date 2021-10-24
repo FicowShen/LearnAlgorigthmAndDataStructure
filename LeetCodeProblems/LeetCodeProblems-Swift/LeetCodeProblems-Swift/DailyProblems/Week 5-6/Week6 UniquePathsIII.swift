6b49bb2f8f5b2ea1c0c55ac776f1145f83f61265
Ficow <945655645@qq.com>
2021-10-24 14:42:09 +0800

    revise;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 UniquePathsIII.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 UniquePathsIII.swift
@@ -45,12 +45,71 @@ final class Week6UniquePathsIII {
 
 
     func dp3(_ grid: [[Int]]) -> Int {
-        fatalError()
+        let Row = grid.count, Col = grid[0].count
+        let drow = [0, 1, 0, -1], dcol = [1, 0, -1, 0]
+        var f = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: -1, count: 1 << (Row * Col)), count: Col), count: Row)
+        var end = (0, 0)
+        func code(r: Int, c: Int) -> Int { 1 << (r * Col + c) }
+        func dp(row: Int, col: Int, target: Int) -> Int {
+            if f[row][col][target] != -1 { return f[row][col][target] }
+            if row == end.0, col == end.1 { return target == 0 ? 1 : 0 }
+            var ans = 0
+            for i in 0..<drow.count {
+                let r = row + drow[i], c = col + dcol[i]
+                if r < 0 || r >= Row || c < 0 || c >= Col { continue }
+                if target & code(r: r, c: c) == 0 { continue }
+                ans += dp(row: r, col: c, target: target ^ code(r: r, c: c))
+            }
+            f[row][col][target] = ans
+            return ans
+        }
+        var target = 0, start = end
+        for r in 0..<Row {
+            for c in 0..<Col {
+                if grid[r][c] & 1 == 0 { target |= code(r: r, c: c) }
+                if grid[r][c] == 1 {
+                    start = (r, c)
+                } else if grid[r][c] == 2 {
+                    end = (r, c)
+                }
+            }
+        }
+        return dp(row: start.0, col: start.1, target: target)
     }
 
 
     func backtrack3(_ grid: [[Int]]) -> Int {
-        fatalError()
+        let Row = grid.count, Col = grid[0].count
+        let drow = [0, 1, 0, -1], dcol = [1, 0, -1, 0]
+        var ans = 0, end = (0, 0), grid = grid
+        func backtrack(row: Int, col: Int, empty: Int) {
+            let empty = empty - 1
+            if row == end.0, col == end.1 {
+                if empty == 0 { ans += 1 }
+                return
+            }
+            grid[row][col] = -1
+            for i in 0..<drow.count {
+                let r = row + drow[i], c = col + dcol[i]
+                if r < 0 || r >= Row || c < 0 || c >= Col { continue }
+                if grid[r][c] == -1 { continue }
+                backtrack(row: r, col: c, empty: empty)
+            }
+            grid[row][col] = 0
+        }
+        var empty = 0, start = end
+        for r in 0..<Row {
+            for c in 0..<Col {
+                if grid[r][c] != -1 { empty += 1 }
+                if grid[r][c] == 1 {
+                    start = (r, c)
+                } else if grid[r][c] == 2 {
+                    end = (r, c)
+                }
+            }
+        }
+        backtrack(row: start.0, col: start.1, empty: empty)
+        return ans
     }
 
 
