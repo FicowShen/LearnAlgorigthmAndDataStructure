957d619a3397779db5ad5f384f9a51d9276035e8
Ficow <945655645@qq.com>
2021-10-29 09:02:58 +0800

    revise;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 FrogJump.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 FrogJump.swift
@@ -20,7 +20,7 @@ import Foundation
  */
 final class Week6FrogJump {
     func run() {
-        let f = canCross2
+        let f = bfs2
         printAndAssert(result: f([0,1,3,5,6,8,12,17]), expected: true)
         printAndAssert(result: f([0,1,2,3,4,8,9,11]), expected: false)
     }
@@ -49,15 +49,64 @@ final class Week6FrogJump {
 
 
     func dp2(_ stones: [Int]) -> Bool {
-        fatalError()
+        let n = stones.count
+        for i in 1..<n {
+            if stones[i] - stones[i - 1] > i { return false }
+        }
+        var dp = [[Bool]](repeating: [Bool](repeating: false, count: n), count: n)
+        dp[0][0] = true
+        for i in 1..<n {
+            for j in stride(from: i - 1, through: 0, by: -1) {
+                let k = stones[i] - stones[j]
+                if k > j + 1 { break }
+                dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1]
+                if i == n - 1, dp[i][k] { return true }
+            }
+        }
+        return false
     }
 
     func dfs2(_ stones: [Int]) -> Bool {
-        fatalError()
+        let n = stones.count
+        var memo = [[Bool?]](repeating: [Bool?](repeating: nil, count: n), count: n)
+        func dfs(i: Int, k: Int) -> Bool {
+            if i == n - 1 { return true }
+            if let v = memo[i][k] { return v }
+            for newK in stride(from: k - 1, through: k + 1, by: 1) {
+                guard newK > 0 else { continue }
+                let newPos = stones[i] + newK, j = stones.firstIndex(of: newPos)
+                if let j = j, dfs(i: j, k: newK) {
+                    memo[i][k] = true
+                    return true
+                }
+            }
+            memo[i][k] = false
+            return false
+        }
+        return dfs(i: 0, k: 0)
     }
 
     func bfs2(_ stones: [Int]) -> Bool {
-        fatalError()
+        let n = stones.count - 1
+        var q = [[0, 0]], memo = Set<[Int]>()
+        while !q.isEmpty {
+            var next = [[Int]]()
+            for pair in q {
+                let i = pair[0], k = pair[1], pos = stones[i]
+                for newK in stride(from: k - 1, through: k + 1, by: 1) {
+                    guard newK > 0, !memo.contains([i, newK]) else { continue }
+                    memo.insert([i, newK])
+                    let newPos = pos + newK
+                    if newPos == stones[n] { return true }
+                    var j = i
+                    while j <= n, stones[j] < newPos { j += 1 }
+                    guard j <= n, stones[j] == newPos else { continue }
+                    next.append([j, newK])
+                }
+            }
+            q = next
+        }
+        return false
     }
 
 
