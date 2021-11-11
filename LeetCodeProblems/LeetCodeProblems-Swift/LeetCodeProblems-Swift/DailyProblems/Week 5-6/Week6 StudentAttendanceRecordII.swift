f239230dcbca8bc89f67c57533fa8b09f917b537
Ficow <945655645@qq.com>
2021-11-11 12:41:56 +0800

    student attendance record ii, recursion;

--- LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 StudentAttendanceRecordII.swift
+++ LeetCodeProblems/LeetCodeProblems-Swift/LeetCodeProblems-Swift/DailyProblems/Week 5-6/Week6 StudentAttendanceRecordII.swift
@@ -9,12 +9,15 @@ import Foundation
 
 /*
  https://leetcode-cn.com/problems/student-attendance-record-ii/
- 1. dp
+ 1. dfs
+ Time: O(n), Space: O(n)
+
+ 2. dp
  Time: O(n), Space: O(n) / O(1)
  */
 final class Week6StudentAttendanceRecordII {
     func run() {
-        let f = compressedDP2
+        let f = checkRecordWithRecursion
         printAndAssert(result: f(2), expected: 8)
         printAndAssert(result: f(1), expected: 3)
         printAndAssert(result: f(10101), expected: 183236316)
@@ -193,8 +196,25 @@ final class Week6StudentAttendanceRecordII {
 
 
 
+    // https://leetcode-cn.com/problems/student-attendance-record-ii/solution/gong-shui-san-xie-yi-ti-san-jie-ji-yi-hu-fdfx/
+    func checkRecordWithRecursion(_ n: Int) -> Int {
+        let mod = Int(1e9) + 7
+        var memo = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: -1, count: 3), count: 2), count: n + 1)
+        func dfs(days: Int, absent: Int, late: Int) -> Int {
+            if absent > 1 || late > 2 { return 0 }
+            if days == 0 { return 1 }
+            if memo[days][absent][late] != -1 { return memo[days][absent][late] }
+            var sum = dfs(days: days - 1, absent: absent + 1, late: 0) % mod // absent
+            sum = (sum + dfs(days: days - 1, absent: absent, late: late + 1)) % mod // late
+            sum = (sum + dfs(days: days - 1, absent: absent, late: 0)) % mod // present
+            memo[days][absent][late] = sum
+            return sum
+        }
+        return dfs(days: n, absent: 0, late: 0)
+    }
 
-    func checkRecord1(_ n: Int) -> Int {
+
+    func checkRecordWithBetterDP(_ n: Int) -> Int {
         let mod = Int(1e9) + 7
         let dp = [[Int]](repeating: [Int](repeating: 0, count: 3), count: 2)
         var f = dp
@@ -225,7 +245,7 @@ final class Week6StudentAttendanceRecordII {
         return sum
     }
 
-    func checkRecord(_ n: Int) -> Int {
+    func checkRecordWithDP(_ n: Int) -> Int {
         let mod = Int(1e9) + 7
         var dp = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: 0, count: 3), count: 2), count: n + 1)
         dp[0][0][0] = 1
